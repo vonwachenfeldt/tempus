f639cc2fd75f28ef20476bb08cf93b6acfbb55fd
David <60789778+vonwachenfeldt@users.noreply.github.com>
2021-03-27 13:07:20 +0100

    Send changes function

--- communications.js
+++ communications.js
@@ -1,1 +1,14 @@
-// Communications with the server goes here
+// Send a client decided change to the server
+function sendChange(event) {
+    console.log(event);
+    if(event === 1) {
+        // Play
+    }
+    if(event === 2) {
+        // Pause
+    }
+    if(event === 3) {
+        // Buffer
+        var currentTimestamp = player.getCurrentTime();
+    }
+}
