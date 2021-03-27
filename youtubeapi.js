d8204f9ba98f100fc53a6a5df4e4ee8f745f6fb5
David <60789778+vonwachenfeldt@users.noreply.github.com>
2021-03-27 13:07:04 +0100

    Event changes now working

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,11 +1,11 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+// 1. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// 2. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
@@ -13,8 +13,8 @@ function onYouTubeIframeAPIReady() {
     height: '390',
     width: '640',
     videoId: 'M7lc1UVf-VE',
-    playerVars : {
-        'autoplay' : 0
+    playerVars: {
+      'autoplay': 0
     },
     events: {
       'onReady': onPlayerReady,
@@ -23,22 +23,17 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
+// 3. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.stopVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-  if(event.data == YT.PlayerState.PLAYING) {
-      console.log("Playing")
-  }
-  if(event.data == YT.PlayerState.PAUSED) {
-      console.log("Paused")
-  }
+
+  // function found in communications.js
+  sendChange(event.data);
+
 }
 function stopVideo() {
   player.stopVideo();
