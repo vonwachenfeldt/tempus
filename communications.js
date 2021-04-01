3a71e0e31c355a4102e486670c7f3c2776f8d636
David <60789778+vonwachenfeldt@users.noreply.github.com>
2021-04-01 22:38:29 +0200

    Fixed cutting of too long text in video preview

--- communications.js
+++ communications.js
@@ -137,9 +137,9 @@ class Connection {
 
                 var toAdd = "";
                 if (newQueueEntry.duration < 1) // Duration is less than one minute 
-                    toAdd = `<div data-id=${newQueueEntry.id} class="video-div"><p class="video"><img class="thumbnail" src="16by9.png"> ${(newQueueEntry.title.length > 60) ? newQueueEntry.title.slice(0, 60) + "..." : newQueueEntry.title} <br><span class="channel-name">by ${newQueueEntry.channel.slice(0, 20) + "..."}</span> (${Math.round(newQueueEntry.duration * 60)} seconds)</p><button onclick="deleteVideo(${newQueueEntry.id})" class="del-video">🗑️</button></div>`;
+                    toAdd = `<div data-id=${newQueueEntry.id} class="video-div"><p class="video"><img class="thumbnail" src="16by9.png"> ${(newQueueEntry.title.length > 60) ? newQueueEntry.title.slice(0, 60) + "..." : newQueueEntry.title} <br><span class="channel-name">by ${(newQueueEntry.channel.length > 20) ? newQueueEntry.channel.slice(0, 20) + "..." : newQueueEntry.channel}</span> (${Math.round(newQueueEntry.duration * 60)} seconds)</p><button onclick="deleteVideo(${newQueueEntry.id})" class="del-video">🗑️</button></div>`;
                 else
-                    toAdd = `<div data-id=${newQueueEntry.id} class="video-div"><p class="video"><img class="thumbnail" src="16by9.png"> ${(newQueueEntry.title.length > 60) ? newQueueEntry.title.slice(0, 60) + "..." : newQueueEntry.title} <br><span class="channel-name">by ${newQueueEntry.channel.slice(0, 20) + "..."}</span> (${Math.round(newQueueEntry.duration)} minutes)</p><button onclick="deleteVideo(${newQueueEntry.id})" class="del-video">🗑️</button></div>`;
+                    toAdd = `<div data-id=${newQueueEntry.id} class="video-div"><p class="video"><img class="thumbnail" src="16by9.png"> ${(newQueueEntry.title.length > 60) ? newQueueEntry.title.slice(0, 60) + "..." : newQueueEntry.title} <br><span class="channel-name">by ${(newQueueEntry.channel.length > 20) ? newQueueEntry.channel.slice(0, 20) + "..." : newQueueEntry.channel}</span> (${Math.round(newQueueEntry.duration)} minutes)</p><button onclick="deleteVideo(${newQueueEntry.id})" class="del-video">🗑️</button></div>`;
 
                 document.getElementById('queue').innerHTML += toAdd;
 
