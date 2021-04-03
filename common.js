a24a7b7210f37268a8a976c639b29754d7236726
Ludvig Magnusson <ludvig.magnusson04@gmail.com>
2021-04-03 18:48:23 +0200

    made queue very mobile friendly

--- common.js
+++ common.js
@@ -1,21 +1,34 @@
 const addVideoToQueueHtml = (video) => {
-    var title = (video.title.length > 50) ? video.title.slice(0, 50) + "..." : video.title;
-    var channel = (video.channel.length > 20) ? video.channel.slice(0, 20) + "..." : video.channel
+    var title = video.title//(video.title.length > 50) ? video.title.slice(0, 50) + "..." : video.title;
+    var channel = video.channel;//(video.channel.length > 20) ? video.channel.slice(0, 20) + "..." : video.channel
     var durationStr = video.duration < 1 ? (Math.round(video.duration * 60) + "s") : (Math.round(video.duration) + " min"); 
     var thumbnail = video.thumbnail.url;
 
     var toAdd = 
-        `<div data-id=${video.id} class="video-div">
-            <p class="video">
+        `<div data-id=${video.id} class="video-div video">
+            <!--<p class="video">-->
                 <img class="thumbnail" src="${thumbnail}">
                 <span class="overlay">${durationStr}</span>
-                <span class="queue-title" onclick="playVideo('${video.id}')">${title}</span>
-                <br>
-                <span class="channel-name">by ${channel}</span>
+                <div class="video-text-container">
+                    <div>
+                        <div class="queue-title line-clamp" onclick="playVideo('${video.id}')">${title}</div>
+                        <div class="channel-name">${channel}</div>
+                    </div>
+                    <div class="video-button-container small">
+                        <button class="mini-button" onclick="playVideo('${video.id}')">▶</button>
+                        <button class="mini-button" onclick="deleteVideo('${video.id}')">🗑️</button>
+                    </div>
+                </div>
+                <div class="video-button-container video-large">
+                    <button class="mini-button" onclick="playVideo('${video.id}')">▶</button>
+                    <button class="mini-button" onclick="deleteVideo('${video.id}')">🗑️</button>
+                </div>
+                <!--<br>
+                <span class="channel-name">${channel}</span>
                 <br>
                 <button class="mini-button" onclick="playVideo('${video.id}')">▶</button>
-                <button class="mini-button" onclick="deleteVideo('${video.id}')">🗑️</button>
-            </p>
+                <button class="mini-button" onclick="deleteVideo('${video.id}')">🗑️</button>-->
+            <!--</p>-->
         </div>`;
 
     document.getElementById('queue').innerHTML += toAdd;
